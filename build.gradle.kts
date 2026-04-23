ab916c3e44a509b76287972dbc338c03b5a0394a
Tsyqax <122242962+tsyqax@users.noreply.github.com>
2026-04-23 23:44:39 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,71 @@
-plugins {
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.aliucord.plugin) apply true
-    alias(libs.plugins.ktlint) apply false
+@file:Suppress("UnstableApiUsage")
+
+import com.aliucord.gradle.AliucordExtension
+import com.android.build.gradle.LibraryExtension
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidExtension
+import org.jlleitschuh.gradle.ktlint.KtlintExtension
+
+subprojects {
+    val libs = rootProject.libs
+
+    apply {
+        plugin(libs.plugins.android.library.get().pluginId)
+        plugin(libs.plugins.aliucord.plugin.get().pluginId)
+        plugin(libs.plugins.kotlin.android.get().pluginId)
+        plugin(libs.plugins.ktlint.get().pluginId)
+    }
+
+    configure<LibraryExtension> {
+        // TODO: Change to your package name
+        namespace = "com.github.yournamehere"
+        compileSdk = 36
+
+        defaultConfig {
+            minSdk = 21
+        }
+
+        buildFeatures {
+            buildConfig = true
+            resValues = true
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_21
+            targetCompatibility = JavaVersion.VERSION_21
+        }
+    }
+
+    configure<AliucordExtension> {
+        // TODO: Change to your name and user ID
+        author("TsyQax", 1011761935043272784L, hyperlink = true)
+
+        // TODO: Change to your repository
+        github("https://github.com/tsyqax/aliucord_plugins_tsq")
+    }
+
+    configure<KtlintExtension> {
+        version.set(libs.versions.ktlint.asProvider())
+
+        coloredOutput.set(true)
+        outputColorName.set("RED")
+        ignoreFailures.set(true)
+    }
+
+    configure<KotlinAndroidExtension> {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_21
+            optIn.add("kotlin.RequiresOptIn")
+        }
+    }
+
+    @Suppress("unused")
+    dependencies {
+        val compileOnly by configurations
+        val implementation by configurations
+
+        compileOnly(libs.discord)
+        compileOnly(libs.aliucord)
+        compileOnly(libs.kotlin.stdlib)
+    }
 }
