a6567a34007c9954a849fe6dca802e2dfb99ea99
Tsyqax <122242962+tsyqax@users.noreply.github.com>
2026-05-26 22:47:40 +0900

    Add build.gradle.kts for FixOnboardingFix plugin
    Initial setup of build.gradle.kts for the FixOnboardingFix plugin.

---
+++ plugins/FixOnboardingFix/build.gradle.kts
@@ -0,0 +1,27 @@
+version = "0.0.1" // Plugin version. Increment this to trigger an update
+description = "Temporary patch while waiting official backport" // Plugin description that will be shown to user
+
+aliucord {
+    // Changelog of your plugin
+    changelog.set(
+        """
+        # 0.0.1
+        * IPR!
+        * refered from FixOnboarding by @scourage_main
+        """.trimIndent(),
+    )
+    // Image or Gif that will be shown at the top of your changelog page
+    // changelogMedia.set("https://cool.png")
+
+    // Add additional authors to this plugin
+    // author("Name", 0L, hyperlink = true)
+    // author("Name", 0L, hyperlink = true)
+
+    // Excludes this plugin from publishing and global plugin repositories.
+    // Set this to false if the plugin is unfinished
+    deploy.set(true)
+
+    // Builds and deploys this plugin but excludes it from global plugin repositories.
+    // Set this if the plugin has reached EOL but a last update should still occur.
+    // deployHidden.set(true)
+}
