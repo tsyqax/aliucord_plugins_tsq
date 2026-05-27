1fb51a42354488bc221269aa908f9b205bbeb81b
Tsyqax <122242962+tsyqax@users.noreply.github.com>
2026-05-27 10:30:40 +0900

    Bump version to 1.0.1 and update changelog

--- plugins/EmojiRank/build.gradle.kts
+++ plugins/EmojiRank/build.gradle.kts
@@ -1,10 +1,13 @@
-version = "1.0.0" // Plugin version. Increment this to trigger an update
+version = "1.0.1" // Plugin version. Increment this to trigger an update
 description = "Make recent category to guild top emojis" // Plugin description that will be shown to user
 
 aliucord {
     // Changelog of your plugin
     changelog.set(
         """
+        # 1.0.1
+        * Fix not displaying bug maybe
+
         # 1.0.0
         * IPR!
         """.trimIndent(),
