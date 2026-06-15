fbdc686c01988d56266a4da71ba88e45b4f1755a
Tsyqax <122242962+tsyqax@users.noreply.github.com>
2026-06-15 14:21:28 +0900

    Add files via upload

---
+++ plugins/AlternativePluginDownloader/src/main/java/com/tsq/plugins/PluginFile.kt
@@ -0,0 +1,75 @@
+/*
+ * This file was extracted from Aliucord Coreplugin and modified.
+ * Copyright (c) 2021 Juby210 & Vendicated
+ * Licensed under the Open Software License version 3.0
+ */
+
+package com.tsq.AlternativePluginDownloader
+
+import com.aliucord.*
+import com.aliucord.entities.Plugin
+import java.io.File
+import java.io.IOException
+
+internal class PluginFile(val plugin: String) : File("${Constants.PLUGINS_PATH}/$plugin.zip") {
+    val isInstalled
+        get() = this.exists()
+
+    fun install(author: String, repo: String, callback: Runnable? = null) {
+        install("https://cdn.jsdelivr.net/gh/$author/$repo@builds/$plugin.zip", callback)
+    }
+
+    fun install(url: String, callback: Runnable? = null) {
+        Utils.threadPool.execute {
+            val safeMode = PluginManager.isSafeModeEnabled()
+            val isReinstall = isInstalled
+			
+            try {
+                Http.simpleDownload(url, this)
+                // Plugins are started on the main thread.
+                // Post to main thread here for consistency.
+                // Otherwise, plugins that create Handler instances or similar will error
+                Utils.mainThread.post {
+                    if (isReinstall) {
+                        PluginManager.stopPlugin(plugin)
+                        PluginManager.unloadPlugin(plugin)
+                    }
+                    if (!safeMode) {
+                        PluginManager.loadPlugin(Utils.appContext, this)
+                        if (PluginManager.isPluginEnabled(plugin))
+                            PluginManager.startPlugin(plugin)
+                        else
+                            PluginManager.enablePlugin(plugin)
+                    }
+                    Utils.showToast("Plugin $plugin successfully ${if (isReinstall) "re" else ""}installed!")
+                    if (safeMode) {
+                        Utils.showToast("Plugin not loaded due to safe mode!")
+                    }
+                    if (PluginManager.plugins[plugin]?.requiresRestart() == true)
+                        Utils.promptRestart()
+
+                    callback?.let { Utils.mainThread.post(it) }
+                }
+            } catch (ex: IOException) {
+                Logger("PluginFile").error(ex)
+                Utils.showToast("Failed to download $plugin: ${ex.message}")
+                if (this.exists()) this.delete()
+            }
+        }
+    }
+
+    fun uninstall(callback: Runnable? = null) {
+        val success = this.delete()
+        Utils.showToast("${if (success) "Successfully uninstalled" else "Failed to uninstall"} $plugin")
+        if (success) {
+            val p = PluginManager.plugins[plugin]
+            PluginManager.stopPlugin(plugin)
+            PluginManager.unloadPlugin(plugin)
+
+            if (p?.requiresRestart() == true)
+                Utils.promptRestart()
+
+            callback?.let { Utils.mainThread.post(it) }
+        }
+    }
+}
