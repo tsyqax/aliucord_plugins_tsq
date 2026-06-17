e3d73a66a711244869e0aefeb962ec7065f7fabd
Tsyqax <122242962+tsyqax@users.noreply.github.com>
2026-06-17 19:21:22 +0900

    Bump version to 1.0.2 and update changelog
    Updated plugin version to 1.0.2 and modified changelog.

--- plugins/FixOnboardingFork/build.gradle.kts
+++ plugins/FixOnboardingFork/build.gradle.kts
@@ -1,4 +1,4 @@
-version = "1.0.1" // Plugin version. Increment this to trigger an update
+version = "1.0.2" // Plugin version. Increment this to trigger an update
 description = "add /onboarding command to do onboarding" // Plugin description that will be shown to user
 
 aliucord {
@@ -7,12 +7,20 @@ aliucord {
         """
         ## NOTE: This is temporary patch while waiting official backport!
 
+        # 1.0.2
+        * Change Title Indicator
+
         # 1.0.1
         * Better Exception?
 
         # 1.0.0
         * IPR!
         * refered from FixOnboarding by @scourage_main
+
+        # JUST THINKING
+        * If you heard there is a better onboarding plugin, I'm so sad.
+        * At least, this plugin works in reality rather than being a mirage.
+        * Then what is worse?
         """.trimIndent(),
     )
     // Image or Gif that will be shown at the top of your changelog page
