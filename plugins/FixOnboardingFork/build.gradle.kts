57e1f303ef87650ab7baf3c376d0c9ecd82b6755
Tsyqax <122242962+tsyqax@users.noreply.github.com>
2026-06-24 14:38:27 +0900

    Bump version to 1.1.1 and update description
    Updated plugin version and description to reflect new onboarding method.

--- plugins/FixOnboardingFork/build.gradle.kts
+++ plugins/FixOnboardingFork/build.gradle.kts
@@ -1,11 +1,15 @@
-version = "1.1.0" // Plugin version. Increment this to trigger an update
-description = "add /onboarding command to do onboarding" // Plugin description that will be shown to user
+version = "1.1.1" // Plugin version. Increment this to trigger an update
+description = "Add Method to do onboarding" // Plugin description that will be shown to user
 
 aliucord {
     // Changelog of your plugin
     changelog.set(
         """
         ## NOTE: This is temporary patch while waiting official backport!
+        # 1.1.1
+        * Now can onboarding from guild menu
+        * Guild Menu = the menu that appears when long-press guild icon
+
         # 1.1.0
         * Better UI: using SettingsPage
         * Support for dark theme: your eyes are now safe
@@ -24,7 +28,6 @@ aliucord {
         # JUST THINKING
         * If you heard there is a better onboarding plugin, I'm so sad.
         * At least, this plugin works in reality rather than being a mirage.
-        * Then what is worse?
         """.trimIndent(),
     )
     // Image or Gif that will be shown at the top of your changelog page
