e139a66a3d91dcd8dde19082b10628b471997328
Tsyqax <122242962+tsyqax@users.noreply.github.com>
2026-07-09 12:07:03 +0900

    Bump plugin version to 1.0.1 and enable deployment

--- plugins/UItweaks/build.gradle.kts
+++ plugins/UItweaks/build.gradle.kts
@@ -1,10 +1,13 @@
-version = "1.0.0" // Plugin version. Increment this to trigger an update
+version = "1.0.1" // Plugin version. Increment this to trigger an update
 description = "Add better UI something" // Plugin description that will be shown to user
 
 aliucord {
     // Changelog of your plugin
     changelog.set(
         """
+        # 1.0.1
+        * Fixed some bug
+
         # 1.0.0
         * IPR!
         * Add ThreadDEL (Add button to delete)
@@ -20,7 +23,7 @@ aliucord {
 
     // Excludes this plugin from publishing and global plugin repositories.
     // Set this to false if the plugin is unfinished
-    deploy.set(false)
+    deploy.set(true)
 
     // Builds and deploys this plugin but excludes it from global plugin repositories.
     // Set this if the plugin has reached EOL but a last update should still occur.
