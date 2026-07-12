95e7cac49b0cefe895f0237484461c3c540b340e
Tsyqax <122242962+tsyqax@users.noreply.github.com>
2026-07-13 08:33:30 +0900

    Bump version to 1.0.2 and update changelog
    Incremented plugin version to 1.0.2 and updated changelog.

--- plugins/UItweaks/build.gradle.kts
+++ plugins/UItweaks/build.gradle.kts
@@ -1,10 +1,13 @@
-version = "1.0.1" // Plugin version. Increment this to trigger an update
+version = "1.0.2" // Plugin version. Increment this to trigger an update
 description = "Add better UI something" // Plugin description that will be shown to user
 
 aliucord {
     // Changelog of your plugin
     changelog.set(
         """
+        # 1.0.2
+        * Add RuleChIcon
+
         # 1.0.1
         * Fixed some bug
 
