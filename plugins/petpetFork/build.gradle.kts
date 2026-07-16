2042dca9c413a8b591186587dc9d6e0e777fc3b4
Tsyqax <122242962+tsyqax@users.noreply.github.com>
2026-07-16 16:28:34 +0900

    Add initial build.gradle.kts for PetpetFork plugin
    Initial setup for the PetpetFork plugin with version and description.

---
+++ plugins/petpetFork/build.gradle.kts
@@ -0,0 +1,29 @@
+version = "1.1.9" // Plugin version. Increment this to trigger an update
+description = "Pet someone" // Plugin description that will be shown to user
+
+aliucord {
+    // Changelog of your plugin
+    changelog.set(
+        """
+        # 1.1.9
+        * IPR!
+        * Impressed from petpet by ThatWolf, Alyaxia
+        """.trimIndent(),
+    )
+    // Image or Gif that will be shown at the top of your changelog page
+    // changelogMedia.set("https://cool.png")
+
+    // Add additional authors to this plugin
+   // author("ThatWolf", 0L, hyperlink = false)
+    //author("Alyaxia", 0L, hyperlink = false)
+    // author("Name", 0L, hyperlink = true)
+
+    // Excludes this plugin from publishing and global plugin repositories.
+    // Set this to false if the plugin is unfinished
+    deploy.set(false)
+
+    // Builds and deploys this plugin but excludes it from global plugin repositories.
+    // Set this if the plugin has reached EOL but a last update should still occur.
+    // deployHidden.set(true)
+}
+
