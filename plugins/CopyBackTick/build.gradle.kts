81fe9d6ae0047e5602c894c54f550f657aca849d
Tsyqax <122242962+tsyqax@users.noreply.github.com>
2026-07-21 20:57:25 +0900

    Bump version to 1.0.1 and update changelog
    Updated version to 1.0.1 and modified changelog.

--- plugins/CopyBackTick/build.gradle.kts
+++ plugins/CopyBackTick/build.gradle.kts
@@ -1,10 +1,13 @@
-version = "1.0.0" // Plugin version. Increment this to trigger an update
+version = "1.0.1" // Plugin version. Increment this to trigger an update
 description = "Make copy when click backtick" // Plugin description that will be shown to user
 
 aliucord {
     // Changelog of your plugin
     changelog.set(
         """
+        # 1.0.1
+        * Rewrited to Kotlin!
+
         # 1.0.0
         * IPR!
         """.trimIndent(),
