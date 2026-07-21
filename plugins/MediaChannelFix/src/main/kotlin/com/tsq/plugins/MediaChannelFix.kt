1c4e44c3767b1e12504df9f2921b29619466f18e
Tsyqax <122242962+tsyqax@users.noreply.github.com>
2026-07-21 20:20:19 +0900

    Rename MediaChannelFix.java to MediaChannelFix.kt

---
+++ plugins/MediaChannelFix/src/main/kotlin/com/tsq/plugins/MediaChannelFix.kt
@@ -0,0 +1,21 @@
+package com.tsq.plugins;
+
+import android.content.Context
+import com.aliucord.annotations.AliucordPlugin
+import com.aliucord.entities.Plugin
+import com.aliucord.patcher.Hook
+import com.discord.api.channel.Channel
+
+@AliucordPlugin
+class MediaChannelFix: Plugin() {
+	override fun start(context: Context) {
+		val getTypeMethod = Channel::class.java.getDeclaredMethod("D")
+		patcher.patch(getTypeMethod, Hook { param ->
+			if (param.result as? Int == 16) {
+				param.result = 15
+			}
+		})
+	}
+
+	override fun stop(context: Context) { patcher.unpatchAll() }
+}
