0c097feed7c78883ebfbfe470233d18bbe06725b
Tsyqax <122242962+tsyqax@users.noreply.github.com>
2026-07-24 20:00:03 +0900

    Bump version to 1.1.5 and update changelog
    Updated plugin version to 1.1.5 and added changelog entry.

--- plugins/FixOnboardingFork/build.gradle.kts
+++ plugins/FixOnboardingFork/build.gradle.kts
@@ -1,10 +1,13 @@
-version = "1.1.4" // Plugin version. Increment this to trigger an update
+version = "1.1.5" // Plugin version. Increment this to trigger an update
 description = "Add Method to do onboarding" // Plugin description that will be shown to user
 
 aliucord {
     // Changelog of your plugin
     changelog.set(
         """
+        # 1.1.5
+        * Add early return for guild check (maybe)
+
         # 1.1.4
         * Add support to change answers after complete onboarding
         * Better answer: if you did not choose, it did not selected
