96ba7202b43704b02df82e63b0f262fe4680c405
Tsyqax <122242962+tsyqax@users.noreply.github.com>
2026-07-28 08:52:16 +0900

    Bump version to 1.2.1 and update changelog
    Updated plugin version to 1.2.1 and modified changelog.

--- plugins/FixOnboardingFork/build.gradle.kts
+++ plugins/FixOnboardingFork/build.gradle.kts
@@ -1,10 +1,13 @@
-version = "1.2.0" // Plugin version. Increment this to trigger an update
+version = "1.2.1" // Plugin version. Increment this to trigger an update
 description = "Add Method to do onboarding" // Plugin description that will be shown to user
 
 aliucord {
     // Changelog of your plugin
     changelog.set(
         """
+        # 1.2.1
+        * Change better subtitle with guildId
+
         # 1.2.0
         * Finally fix auto mode, oh yea maybe?
         * If you still have bug, pls report to me!
