37adeb282587fc38f118d9084f3c9081f5496003
TsyQax <122242962+tsyqax@users.noreply.github.com>
2026-08-20 09:22:29 +0900

    Bump version to 1.2.4 and update changelog
    Updated version to 1.2.4 and added changelog entry.

--- plugins/MosaicFork/build.gradle.kts
+++ plugins/MosaicFork/build.gradle.kts
@@ -1,10 +1,13 @@
-version = "1.2.3" // Plugin version. Increment this to trigger an update
+version = "1.2.4" // Plugin version. Increment this to trigger an update
 description = "Make image to grid gird" // Plugin description that will be shown to user
 
 aliucord {
     // Changelog of your plugin
     changelog.set(
         """
+        # 1.2.4
+        * Fix bug with padding
+
         # 1.2.3
         * Rewrite to Kotlin!
         * by lazy
