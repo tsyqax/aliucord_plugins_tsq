18e4d16790e864da74a6365924c97ca5046ff083
TsyQax <122242962+tsyqax@users.noreply.github.com>
2026-08-21 15:03:41 +0900

    Bump version to 1.2.5 and update changelog
    Updated plugin version to 1.2.5 and modified changelog.

--- plugins/ForumTagFix/build.gradle.kts
+++ plugins/ForumTagFix/build.gradle.kts
@@ -1,10 +1,13 @@
-version = "1.2.4" // Plugin version. Increment this to trigger an update
+version = "1.2.5" // Plugin version. Increment this to trigger an update
 description = "Fix issues with Tags on ForumTag" // Plugin description that will be shown to user
 
 aliucord {
     // Changelog of your plugin
     changelog.set(
         """
+        # 1.2.5
+        * Change Indicator text color
+
         # 1.2.4
         * Rewrite to Kotlin!
         * by lazy
