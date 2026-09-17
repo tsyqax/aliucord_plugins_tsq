b6c8e41f218883c7643371f6fd85c23f64430ea4
TsyQax <122242962+tsyqax@users.noreply.github.com>
2026-09-18 03:03:53 +0900

    Bump plugin version to 1.3.2
    Updated version to 1.3.2 and modified changelog.

--- plugins/FixOnboardingFork/build.gradle.kts
+++ plugins/FixOnboardingFork/build.gradle.kts
@@ -1,10 +1,13 @@
-version = "1.3.1" // Plugin version. Increment this to trigger an update
+version = "1.3.2" // Plugin version. Increment this to trigger an update
 description = "Add Method to do onboarding" // Plugin description that will be shown to user
 
 aliucord {
     // Changelog of your plugin
     changelog.set(
         """
+        # 1.3.2
+        * Some slightly change
+
         # 1.3.1
         * Fix bug when using background image with theme
         * UI uses SettingPage: I'm so stupid
