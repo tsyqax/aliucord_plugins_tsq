433bcae8ea9dcc43f6e2cb48bd4a9de0f71477d3
TsyQax <122242962+tsyqax@users.noreply.github.com>
2026-09-20 18:44:27 +0900

    Bump version to 1.2.6 and update changelog
    Updated plugin version to 1.2.6 and added changelog entry for the new release.

--- plugins/MosaicFork/build.gradle.kts
+++ plugins/MosaicFork/build.gradle.kts
@@ -1,10 +1,13 @@
-version = "1.2.5" // Plugin version. Increment this to trigger an update
+version = "1.2.6" // Plugin version. Increment this to trigger an update
 description = "Make image to grid gird" // Plugin description that will be shown to user
 
 aliucord {
     // Changelog of your plugin
     changelog.set(
         """
+        # 1.2.6
+        * Oh my head: Fix squareMode
+
         # 1.2.5
         * Redesign grid formula
         * Improve adapting dynamic screenWidth
